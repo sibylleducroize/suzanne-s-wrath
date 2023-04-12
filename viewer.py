0cf0875aa8c69ec194d75d165a7c6df964db7e03
Sibylle Ducroize <sibylle.ducroize@grenoble-inp.org>
2023-04-12 14:50:19 +0200

    switching

--- viewer.py
+++ viewer.py
@@ -5,39 +5,7 @@ import OpenGL.GL as GL              # standard Python OpenGL wrapper
 import glfw                         # lean window system wrapper for OpenGL
 import numpy as np                  # all matrix manipulations & OpenGL args
 from core import Shader, Viewer, Mesh, load
-from texture import Texture, Textured
-
-
-# -------------- Example textured plane class ---------------------------------
-class TexturedPlane(Textured):
-    """ Simple first textured object """
-    def __init__(self, shader, tex_file):
-        # prepare texture modes cycling variables for interactive toggling
-        self.wraps = cycle([GL.GL_REPEAT, GL.GL_MIRRORED_REPEAT,
-                            GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
-        self.filters = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
-                              (GL.GL_LINEAR, GL.GL_LINEAR),
-                              (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
-        self.wrap, self.filter = next(self.wraps), next(self.filters)
-        self.file = tex_file
-
-        # setup plane mesh to be textured
-        base_coords = ((-1, -1, 0), (1, -1, 0), (1, 1, 0), (-1, 1, 0))
-        scaled = 100 * np.array(base_coords, np.float32)
-        indices = np.array((0, 1, 2, 0, 2, 3), np.uint32)
-        mesh = Mesh(shader, attributes=dict(position=scaled), index=indices)
-
-        # setup & upload texture to GPU, bind it to shader name 'diffuse_map'
-        texture = Texture(tex_file, self.wrap, *self.filter)
-        super().__init__(mesh, diffuse_map=texture)
-
-    def key_handler(self, key):
-        # cycle through texture modes on keypress of F6 (wrap) or F7 (filtering)
-        self.wrap = next(self.wraps) if key == glfw.KEY_F6 else self.wrap
-        self.filter = next(self.filters) if key == glfw.KEY_F7 else self.filter
-        if key in (glfw.KEY_F6, glfw.KEY_F7):
-            texture = Texture(self.file, self.wrap, *self.filter)
-            self.textures.update(diffuse_map=texture)
+from texture import Texture, Textured, TexturedPlane
 
 
 # -------------- main program and scene setup --------------------------------
